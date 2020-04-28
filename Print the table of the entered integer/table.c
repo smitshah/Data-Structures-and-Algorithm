7041aaae607969db5cb4aac1f1811ad462175317
Smit Shah <smitshah0885@gmail.com>
2020-04-28 13:41:59 +0530

    Update table.c

--- Print the table of the entered integer/table.c
+++ Print the table of the entered integer/table.c
@@ -49,3 +49,29 @@ i++;
 }while(i<=10);
 return 0;
 }
+
+
+// (d) recursive function
+
+#include <stdio.h>
+#include <stdlib.h>
+int mul(int n, int i);
+int main()
+{
+int n, i=1;
+printf("\n Enter the number: ");
+scanf("%d", &n);
+mul(n, i);
+}
+int mul(int n, int i)
+{
+if(i>10)
+{
+return 0;
+}
+else
+{
+printf("%d * %d = %d \n", n, i, n*i);
+return mul(n, i+1);
+}
+}
